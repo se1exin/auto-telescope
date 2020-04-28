0a937fd81ac7b82dceabc84df08132a2bd6d206e
se1exin <selexin@selexin.com>
2020-04-28 19:40:21 +1000

    Change MPU9250 library back to mpu9250_jmdev. Add AHRS python library for madgwick attitude calculation

--- api/api.py
+++ api/api.py
@@ -16,19 +16,21 @@ CORS(app)
 
 stepper = Stepper()
 imu = IMU()
-imu.init()
+imu.configure()
 
 
 @app.route("/calibrate", methods=["POST"])
 def calibrate():
+	imu.stop_updating()
+
 	# To calibrate the mag we need to move the sensor around.
 	# The best we can do is just spin the motor as fast as we can during calibration
 	stepper.enable()
 	stepper.set_mode_full_step()
 
 	# Start calibration in another thread so we can rotate the motor in this thread.
-	x = threading.Thread(target=imu.calibrate_mag)
-	x.start()
+	# x = threading.Thread(target=imu.calibrate_mag)
+	# x.start()
 
 	while imu.is_calibrating_mag:
 		stepper.step_forward()
@@ -38,25 +40,77 @@ def calibrate():
 	stepper.disable()
 
 	# The rest of the sensors (accel, gyro) must be calibrated while not moving
-	time.sleep(1)
+	# time.sleep(1)
 	imu.calibrate_mpu()
 
+	imu.configure()
+
 	# Calibration function resets the sensors, so we need to reconfigure them
-	return jsonify({'success': True})
+	return jsonify({
+		'success': True,
+		'magBias': imu.mpu9250.magBias,
+		'magScale': imu.mpu9250.magScale
+	})
 
 
 @app.route("/imu/start", methods=["POST"])
-def start_imu():
+def imu_start():
 	if imu.updating:
 		return jsonify({'success': False})
 
-	x = threading.Thread(target=imu.update, daemon=True)
+	x = threading.Thread(target=imu.start_updating, daemon=True)
 	x.start()
 	return jsonify({'success': True})
+
+
+@app.route("/imu/stop", methods=["POST"])
+def imu_stop():
+	if not imu.updating:
+		return jsonify({'success': False})
+
+	imu.stop_updating()
+	return jsonify({'success': True})
+
+@app.route("/imu/mag/dump", methods=["POST"])
+def imu_mag_dump():
+	import csv
+	import matplotlib.pyplot as plt
+	# Take a bunch of raw mag samples and dump them to file for analysis
+	readings = list()
+	x = list()
+	y = list()
+	z = list()
+	time_step = 0.01
+	sample_duration = int(15 / time_step)
+	for _ in range(0, sample_duration):
+		mag_data = imu.mpu9250.raw_mag
+		print(mag_data)
+		if mag_data is not None:
+			readings.append(mag_data)
+			x.append(mag_data[0])
+			y.append(mag_data[1])
+			z.append(mag_data[2])
+		time.sleep(time_step)
+
+	writer = csv.writer(open("mag_dump.csv", 'w'))
+	for row in readings:
+		writer.writerow(row)
+
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+	ax1.scatter(x, y, marker='.', label="XY")
+	ax1.scatter(x, z, marker='.', label="XY")
+	ax1.scatter(y, z, marker='.', label="YZ")
+	plt.legend(loc='upper left')
+	plt.savefig('static/my_plot.png')
+
+	return jsonify({'success': True})
+
 
 @app.route("/position")
 def get_position():
 	return jsonify(imu.get_position())
+
 
 @app.route("/position", methods=["POST"])
 def update_position():
@@ -66,11 +120,49 @@ def update_position():
 	stepper.enable()
 	stepper.set_mode_half_step()
 
-	mag_pos = imu.yaw
-	while mag_pos != pos_x:
-		print(mag_pos)
-		stepper.step_forward()
+	# Mag algorithm can take a few seconds to stabilise after movement
+	# To move to the target position, we first move to an estimated position,
+	# then wait for stabilisation and see how far off we are.
+	# We keep doing this until we hit the target
+
+	are_we_there_yet = False
+
+	while not are_we_there_yet:
 		mag_pos = imu.yaw
+		distance = pos_x - mag_pos
+		rotate_time = 5000
+
+		# We can't accurately estimate how to rotate the stepper yet.
+		# To do so we need to direct connection to the stepper motor driver (currently over I2C to an arduino)
+		# and we also need to know the gear ratios.
+		# With a direct connection and known gear ratios we should be able to predict and turn the stepper pretty
+		# close to the target direction... but for now we will just use the following basic narrow-down logic.
+
+		# As the distance gets small, slow down the motor speed and time between rotation/stabilisation attempts
+		if abs(distance) > 90:
+			stepper.set_mode_full_step()
+		elif abs(distance) > 30:
+			stepper.set_mode_half_step()
+			rotate_time = 2000
+		elif abs(distance) > 5:
+			stepper.set_mode_quarter_step()
+			rotate_time = 1000
+		else:
+			stepper.set_mode_eigth_step()
+			rotate_time = 500
+
+		start_time = time.time() * 1000
+
+		while ((time.time() * 1000) - start_time) < rotate_time:
+			if distance < 0:
+				stepper.step_reverse()
+			else:
+				stepper.step_forward()
+
+		time.sleep(3)  # Wait for mag stabilisation
+		mag_pos = imu.yaw
+
+		are_we_there_yet = int(mag_pos) == pos_x
 
 	stepper.disable()
 
--- api/imu/imu.py
+++ api/imu/imu.py
@@ -1,49 +1,111 @@
 import sys
+
+from ahrs.common import DEG2RAD, RAD2DEG
+from ahrs.common.orientation import q2euler
+
 sys.path.append("")
 
 import time
-from MPU9250.mpu9250 import MPU9250
+from ahrs.filters import Madgwick
+from mpu9250_jmdev.registers import *
+from mpu9250_jmdev.mpu_9250 import MPU9250
 
 
 class IMU(object):
+    is_calibrating_mag = False
 
     def __init__(self):
-        self.mpu9250 = MPU9250()
+        self.mpu9250 = MPU9250(
+            address_ak=AK8963_ADDRESS,
+            address_mpu_master=MPU9050_ADDRESS_68,
+            address_mpu_slave=None,
+            bus=1,
+            gfs=GFS_1000,
+            afs=AFS_8G,
+            mfs=AK8963_BIT_16,
+            mode=AK8963_MODE_C100HZ)
+
         self.is_calibrating_mag = False
-        self.yaw = 0.0
-        self.pitch = 0.0
-        self.roll = 0.0
         self.updating = False
 
-    def init(self):
-        self.mpu9250.initMPU9250()
-        print("MPU9250 initialized for active data mode....")
-        self.mpu9250.initAK8963()
-        print("AK8963 initialized for active data mode....")
+        self.mpu9250.magBias = [20.7454594017094,
+                                10.134935897435899,
+                                -11.132967032967032]
+        self.mpu9250.magScale = [1.0092850510677809,
+                                1.0352380952380953,
+                                0.9585537918871252]
+
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
+
+    def configure(self):
+        self.mpu9250.configure()
 
     def calibrate_mag(self):
         self.is_calibrating_mag = True
-        self.mpu9250.magCalMPU9250()
+        self.mpu9250.calibrateAK8963()
         self.is_calibrating_mag = False
 
     def calibrate_mpu(self):
-        self.mpu9250.calibrateMPU9250()
+        self.mpu9250.calibrateMPU6500()
+
+    def stop_updating(self):
+        self.updating = False
+
+    def start_updating(self):
+        # Calculate AHRS
+        if self.updating:
+            return
+
+        self.updating = True
+        madgwick = Madgwick(frequency=50, beta=1)
+        q = [1, 0, 0, 0]
+        while self.updating:
+            accel = self.mpu9250.readAccelerometerMaster()
+            gyro = list(map(lambda x: x * DEG2RAD, self.mpu9250.readGyroscopeMaster()))
+            mag = self.mpu9250.readMagnetometerMaster()
+            q = madgwick.updateMARG(acc=accel, gyr=gyro, mag=mag, q=q)
+
+            attitude = q2euler(q)
+            self.roll = attitude[0] * RAD2DEG
+            self.pitch = attitude[1] * RAD2DEG
+            self.yaw = attitude[2] * RAD2DEG
+
+            print(self.yaw)
+
+            # print("roll %f, pitch %f, yaw %f" % (self.roll, self.pitch, self.yaw))
+            time.sleep(0.02)
+
+    def get_accel(self):
+        data = self.mpu9250.readAccelerometerMaster()
+        return {
+            'x': data[0],
+            'y': data[1],
+            'z': data[2],
+        }
+
+    def get_gyro(self):
+        data = self.mpu9250.readGyroscopeMaster()
+        return {
+            'x': data[0],
+            'y': data[1],
+            'z': data[2],
+        }
+
+    def get_mag(self):
+        data = self.mpu9250.readMagnetometerMaster()
+        return {
+            'x': data[0],
+            'y': data[1],
+            'z': data[2],
+        }
 
     def get_position(self):
         return {
-            'yaw': self.yaw,
-            'pitch': self.pitch,
-            'roll': self.roll,
+            'mag': self.get_mag(),
+            'gyro': self.get_gyro(),
+            'accel': self.get_accel()
         }
 
-    def update(self):
-        self.updating = True
-        while True:
-            try:
-                self.mpu9250.update()
-                result = self.mpu9250.euler
-                self.roll = result[0]
-                self.pitch = result[1]
-                self.yaw = result[2]
-            except:
-                pass
+
